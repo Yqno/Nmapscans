7804a4fc90a21b7b8eea6cc5b7159bdc703599d4
Y Q N O <79216939+Yqno@users.noreply.github.com>
2023-02-16 12:28:45 +0100

    Update normalscan.sh

--- normalscan.sh
+++ normalscan.sh
@@ -3,6 +3,6 @@
 echo "Enter the target IP address: "
 read target_ip
 
-nmap -A -T4 $target_ip
+nmap -Pn -A -T4 $target_ip
 
 # performs a basic portscan using nmap with the flags -A detect OS and vulnerabilities and -T4 set timing template to aggressive
